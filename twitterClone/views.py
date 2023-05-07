8a10024c259ab26ac438ab9ba89880454b5cca47
Emebet Tilahun <emuaniley@gmail.com>
2023-05-07 08:56:30 +0300

    image

--- twitterClone/views.py
+++ twitterClone/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
-from .forms import MeepForm
-
+from .forms import MeepForm, SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
 # Create your views here.
 def home(request):
     if request.user.is_authenticated:
@@ -41,4 +44,60 @@ def profile(request,pk):
         return render(request, "profile.html", {"profile":profile,"meeps":meeps})
     else:
         return redirect('home')
+    
+def login_user(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You Have Been Logged In! "))
+            return redirect('home')
+        else:
+             messages.success(request, ("There Was an Error Please Try Again! "))
+             return redirect('login')
+
+    else:
+        
+        return render(request, "login.html", {})
+
+def logout_user(request):
+    
+    logout(request)
+    messages.success(request, ("You Have Been Logged Out! "))
+    return redirect('home')
+
+def register_user(request):
+     form=SignUpForm()
+     if request.method=="POST":
+         form=SignUpForm(request.POST)
+         if form.is_valid():
+             form.save()
+             username=form.cleaned_data['username']
+             password=form.cleaned_data['password1']
+            #  first_name=form.cleaned_data['first_name']
+            #  last_name=form.cleaned_data['last_name']
+            #  email=form.cleaned_data['email']
+             user=authenticate(username=username,password=password)
+             login(request,user)
+             messages.success(request, ("You Registered Successfully! "))
+             return redirect('home')
+     return render(request, "register.html", {'form':form})
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user= User.objects.get(id=request.user.id)
+        form=SignUpForm(request.POST or None, instance=current_user)
+        if form.is_valid():
+            form.save()
+            login(request,current_user)
+            messages.success(request, ("Your Profile Upadated! "))
+            return redirect('home')
+        return render(request, "update_user.html", {'form':form})
+    else:
+        messages.success(request, ("You Must Be login! "))
+        return redirect('home')
+
     
